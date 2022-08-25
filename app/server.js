610497c852a4dbf8bf1a579a3da7c5df331a262d
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-08-25 23:10:27 +0100

    Revert "learning pug"
    This reverts commit f5b087592d0cf1a898cc4344b89cd55ec34342c9.

--- app/server.js
+++ app/server.js
@@ -6,19 +6,18 @@ const PORT = 80;
 
 const { name, description, title } = require("./config.json");
 const cursus = require("./cursus.json");
-const informations = require("./informations.json");
 
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "pug");
 
 app.use(express.static(__dirname));
 
-app.get("/index.html", (_req, res) => {
+app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.get("/", (_req, res) => {
-  res.render("cv", { title, name, description, cursus, informations });
+app.get("/pug", (_req, res) => {
+  res.render("index", { title, name, description, cursus });
 });
 
 app.listen(PORT, () => {
