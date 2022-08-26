147e0318b5d5d1699070111579295ebeb99b84bf
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-08-26 14:13:19 +0100

    add json db

--- app/server.js
+++ app/server.js
@@ -1,26 +1,27 @@
 const path = require("path");
 
 const express = require("express");
-const app = express();
-const PORT = 80;
+const axios = require("axios");
 
-const { name, description, title } = require("./config.json");
-const cursus = require("./cursus.json");
-const informations = require("./informations.json");
+const app = express();
+const PORT = 8080;
 
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "pug");
 
 app.use(express.static(__dirname));
 
-app.get("/index.html", (_req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
-
-app.get("/", (_req, res) => {
-  res.render("cv", { title, name, description, cursus, informations });
+app.get("/", async (_req, res) => {
+  const data = await axios
+    .get("http://localhost:3000/db")
+    .then((res) => res.data)
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+  res.render("cv", data);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening: http://localhost:8080`);
 });
