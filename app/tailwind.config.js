73367579fd9e1f58c17c39c9724b05a01d9fae07
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-08-29 15:50:12 +0100

    separate server and db

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/views/**/*.pug"],
+  content: ["/views/**/*.pug"],
   theme: {
     extend: {
       colors: {
--- database/index.js
+++ database/index.js
@@ -1,11 +1,12 @@
 const { readdirSync } = require("fs");
 const { join, basename } = require("path");
 
+const pathToJSON = (file = "") => join(__dirname, "/json", file);
+
 module.exports = () => {
   const db = {};
-  readdirSync(__dirname).forEach((file) => {
-    if (file === basename(__filename)) return;
-    const filePath = join(__dirname, file);
+  readdirSync(pathToJSON()).forEach((file) => {
+    const filePath = pathToJSON(file);
     db[basename(file, ".json")] = require(filePath);
     console.log("loaded:", filePath);
   });
