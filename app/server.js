3f03c2feea4a3ef8113d29fe79a7db1823453e89
Ulysse Chopard-Guillaumot <82863549+UlysseChopard@users.noreply.github.com>
2022-09-03 14:50:34 +0200

    Pug (#1)
    * create project
    
    * save work
    
    * style and content
    
    * fix style
    
    * add photo
    
    * make it dynamic
    
    * refacto
    
    * refacto
    
    * rename files
    
    * gitignore
    
    * refacto
    
    * end of refacto
    
    * clean code
    
    * only js
    
    * init npm
    
    * static folder
    
    * works with nodemon
    
    * mv static app
    
    * add pug
    
    * learning pug
    
    * clean branch
    
    * add json db
    
    * improve appearance
    
    * use dynamic loading for json server
    
    * refacto template
    
    * modify style
    
    * style and pug refacto
    
    * separate server and db
    
    * add sidebar
    
    * fix height in nav
    
    * rm right sidebar
    
    * nav managed by js
    
    * navba works
    
    * nav bar works 2
    
    * add software and misc
    
    * change navigation
    
    * beautiful misc infos

---
+++ app/server.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+const express = require("express");
+const axios = require("axios");
+
+const viewConfig = require("./config.json");
+
+const app = express();
+const PORT = 8080;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.locals = {
+  config: viewConfig,
+  basedir: app.get("views"),
+};
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (_req, res) => {
+  const data = await axios
+    .get("http://localhost:3000/db")
+    .then((res) => res.data)
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+  res.render("pages/cv", data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening: http://localhost:8080`);
+});
