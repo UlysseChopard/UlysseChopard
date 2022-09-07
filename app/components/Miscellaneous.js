0b7a26054f155cd1a80d271747176f87046c9228
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-09-07 23:36:02 +0100

    add missing key

--- app/components/Miscellaneous.js
+++ app/components/Miscellaneous.js
@@ -2,7 +2,7 @@ export default function Miscellaneous({ miscellaneous }) {
   return (
     <div id="miscellaneous" className="flex flex-wrap justify-center">
       {miscellaneous.map((category) => (
-        <div className="rounded-lg m-4 p-4 w-60 shadow-md">
+        <div key={category} className="rounded-lg m-4 p-4 w-60 shadow-md">
           <h2 className="uppercase font-semibold text-lg text-secondary">
             {category.title}
           </h2>
